aacc10d5b7d484afce72b92047547d743217032c
SohnNim <cubemackerel@naver.com>
2018-11-27 15:10:30 +0900

    Use operation filter, trans

--- Main.scala
+++ Main.scala
@@ -15,7 +15,14 @@ object Main {
 
     val work1 = Work1(List(1, 2, 3, 4, 5), option)
     val work2 = Work2(List("a", "b", "c", "d", "e"), option)
-
     assert(work1.data.length == work2.data.length)
+
+    val filteredWork1 = work1.filterHalf
+    val filteredWork2 = work2.filterHalf
+    assert(filteredWork1.data.length == filteredWork2.data.length)
+
+    val transedWork1 = filteredWork1.transData
+    val transedWork2 = filteredWork2.transData
+    assert(transedWork1.data.length == transedWork2.data.length)
   }
 }

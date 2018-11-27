e5bd499a2cd4deb083eda2b6113572d1c1eb3fef
SohnNim <cubemackerel@naver.com>
2018-11-27 15:06:54 +0900

    Add filterHalf Logic for Work1, Work2

--- Work1.scala
+++ Work1.scala
@@ -1,7 +1,21 @@
 package com.sparrow.gittest.works
 
-import com.sparrow.gittest.option.MyOption
+import com.sparrow.gittest.option.{MyOption, OptionA, OptionB}
 
 case class Work1(data: List[Int], option: MyOption) {
+
+  def filterHalf: Work1 = {
+    option match {
+      case OptionA => {
+        val oddFilteredData = data.zipWithIndex.filter(_._2 % 2 == 0).map(_._1)
+        this.copy(data = oddFilteredData)
+      }
+      case OptionB => {
+        val evenFilteredData = data.zipWithIndex.filter(_._2 % 2 == 1).map(_._1)
+        this.copy(data = evenFilteredData)
+      }
+      case _ => this
+    }
+  }
 
 }
